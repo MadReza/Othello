4c57c89ae669b8abe6531349c624b31fbbd7bae8
Ahmad Madabadi <rezmaza@gmail.com>
2016-10-25 17:20:41 -0400

    Initial File and Added emacs temps to the gitignore

---
+++ Othello.py
@@ -0,0 +1,11 @@
+print "Othello Start"
+
+print "Display: "
+
+print "________________"
+print "[ O | O | O | O ]"
+print "[ 0 | W | B | O ]"
+print "[ 0 | B | W | O ]"
+print "[ O | O | O | O ]"
+print "________________"
+

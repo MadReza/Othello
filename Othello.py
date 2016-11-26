c6d0eb6cd13628beed04caac16dbc071bd93e1d3
Ahmad Madabadi <a_madab@encs.concordia.ca>
2016-11-26 16:58:44 -0500

    Cleaned & Added get_possible_moves()

--- Othello.py
+++ Othello.py
@@ -14,16 +14,14 @@ class Othello:
 
     W = "W"
     B = "B"
-    row = 8
+    row = 8     #Eventually upgrade to adjustable board
     col = 8
     BOARD_TOTAL_MOVES = row * col    
-    cur_moves = 0
+     
     
     def __init__(self):
         print "Othello start"
         self.__reset_board__()
-        self.turn = self.B
-        self.score = 0   #+1 for Black Pieces -1 for White Pieces.
 
     def __reset_board__(self):
         self.board = [[' ' for x in range(self.col)] for y in range(self.row)]
@@ -32,7 +30,10 @@ class Othello:
         self.board[3][4] = self.W
         self.board[4][3] = self.W
         self.cur_moves = 4
-
+        self.history = ()   #Takes Tuples of (x,y) values with Black be first
+        self.turn = self.B
+        self.score = 0   #+1 for Black Pieces -1 for White Pieces.
+        
     def __str__(self):
         s = "    0   1   2   3   4   5   6   7  " + "\n"
         s = s + "  ---------------------------------" + "\n"
@@ -63,12 +64,19 @@ class Othello:
         self.cur_moves = self.cur_moves + 1
         self.__switch_turn__()
 
+    def get_possible_moves(self):
+        playable = []
+
+        for col in range(0, self.col):
+            for row in range(0, self.row):
+                if self.board[row][col] == " " and self.is_playable(col, row):
+                    playable.append((col, row))
+        return playable
 
     def is_playable(self, col, row):
         if self.out_of_bounds(col, row):
             return False
         if self.board[row][col] != " ":
-            #raise IndexError("Key Already There")
             return False
         
         for dir_col in range(-1, 2):
@@ -116,13 +124,11 @@ class Othello:
         row = start_row + dir_row
         col = start_col + dir_col
 
-        if self.board[row][col] != jump:
+        if self.out_of_bounds(col, row) or self.board[row][col] != jump:
             return False
 
         while True:
-            if self.out_of_bounds(col, row):
-                return False
-            if self.board[row][col] == " ":
+            if self.out_of_bounds(col, row) or self.board[row][col] == " ":
                 return False
             if self.board[row][col] == target:
                 """Found Sandwich of tiles"""
@@ -133,5 +139,5 @@ class Othello:
 
 x = Othello()
 print x
-
+print x.get_possible_moves()
 
